f553c61162b41292aada7cc784a3b8a7f144dce5
guoruixiao <grx0917@sina.com>
2019-07-24 16:06:49 +0800

    Initial commit

---
+++ JLRoutesDemo/Base/UIViewController/ffffViewController.h
@@ -0,0 +1,17 @@
+//
+//  ffffViewController.h
+//  JLRoutesDemo
+//
+//  Created by grx on 2019/7/24.
+//  Copyright © 2019 Destiny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ffffViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JLRoutesDemo/Base/UIViewController/ffffViewController.m
@@ -0,0 +1,33 @@
+//
+//  ffffViewController.m
+//  JLRoutesDemo
+//
+//  Created by grx on 2019/7/24.
+//  Copyright © 2019 Destiny. All rights reserved.
+//
+
+#import "ffffViewController.h"
+
+@interface ffffViewController ()
+
+@end
+
+@implementation ffffViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
